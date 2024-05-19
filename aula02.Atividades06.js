8fc81eb9ba605536cfbf8eb954a77e70e67b70ff
Andersonfrota1988 <143961469+Andersonfrota1988@users.noreply.github.com>
2024-05-19 13:19:29 -0300

    aula02.Atividades06.js

--- aula02.Atividades06.js
+++ aula02.Atividades06.js
@@ -2,23 +2,20 @@ let num = Number(prompt(`Digite um número qualquer:`))
 
 if (num  <= 1){
     console.log(num + "não é um número primo.")
-} 
+} else{
+  let primo = true
+  let divisor = 2
 
-else{
-    let primo = true
-    let divisor = 2
-
-    while (divisor * divisor <= num && primo){
-        if (num % divisor === 0)
-            primo = false
+  while (divisor * divisor <= num && primo){
+    if (num % divisor === 0) {
+      primo = false
     }
     divisor ++
 }
 
-if (primo){
-    console.log(num + "é um número primo.")
-} else {
-        console.log(num + "não é um número primo.")
-
-    
+ if (primo){
+  console.log(num + "é um número primo.")
+ } else {
+  console.log(num + "não é um número primo.")
+ }  
 }
